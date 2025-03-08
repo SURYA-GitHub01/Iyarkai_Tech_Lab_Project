8f5379e188c96cfd2210deb283d33536fad58f89
SURYA S <140172921+SURYA-GitHub01@users.noreply.github.com>
2025-03-08 14:49:14 +0530

    Delete Project directory

--- Project/src/main/java/com/IyarkaiGreenCommune/Project/Repo/LoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.IyarkaiGreenCommune.Project.Repo;
-
-import com.IyarkaiGreenCommune.Project.Entity.LoginEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRepo extends JpaRepository<LoginEntity,Integer> {
-}
